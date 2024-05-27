ddd9b1f0817bacdf9b1a37ccd49e1ebf8525a5bc
anushac96 <anushac96@gmail.com>
2024-05-27 14:17:14 -0700

    added another aproach

--- LeetCode75/src/longestZigZagPathInABinaryTree/LongestZigZagPathInABinaryTree.java
+++ LeetCode75/src/longestZigZagPathInABinaryTree/LongestZigZagPathInABinaryTree.java
@@ -2,8 +2,8 @@ package longestZigZagPathInABinaryTree;
 
 public class LongestZigZagPathInABinaryTree {
 
-	int count = 0;
-
+	int count1 = 0;
+	int count2 = 0;
 	public static void main(String[] args) {
 		TreeNode root = new TreeNode(1,
 				null,
@@ -17,28 +17,43 @@ public class LongestZigZagPathInABinaryTree {
 												new TreeNode(8))),
 								new TreeNode(6))));
 		LongestZigZagPathInABinaryTree p = new LongestZigZagPathInABinaryTree();
-		System.out.println(p.longestZigZag(root));
+		System.out.println(p.longestZigZag1(root));
+		System.out.println(p.longestZigZag2(root));
 	}
 
-	public int longestZigZag(TreeNode root) {
-		travel(root, 0, false);
-		travel(root, 0, true);
-		return count;
+	public int longestZigZag1(TreeNode root) {
+		travel1(root, 0, false);
+		travel1(root, 0, true);
+		return count1;
 	}
 
-	public void travel(TreeNode root, int step, boolean goLeft) {
+	public void travel1(TreeNode root, int step, boolean goLeft) {
 		if (root == null) {
 			return;
 		}
-		count = Math.max(count, step);
+		count1 = Math.max(count1, step);
 		if (goLeft) {
-			travel(root.left, step + 1, false);
-			travel(root.right, 1, true);
+			travel1(root.left, step + 1, false);
+			travel1(root.right, 1, true);
 		} else {
-			travel(root.right, step + 1, true);
-			travel(root.left, 1, false);
+			travel1(root.right, step + 1, true);
+			travel1(root.left, 1, false);
 		}
 	}
+	
+	public int longestZigZag2(TreeNode root) {
+        travel2(root,0,0);
+        return count2;
+    }
+
+    public void travel2(TreeNode root, int l, int r){
+        if(root==null){
+            return;
+        }
+        count2 = Math.max(count2,Math.max(l,r));
+        travel2(root.left,r+1,0);
+        travel2(root.right,0,l+1);
+    }
 }
 
 class TreeNode {
